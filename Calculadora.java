e18aa51c73038ed4a902854fe8dd635e530b5808
GabrielPerera <gabrielpereira0907@gmail.com>
2024-01-22 13:57:53 -0300

    Resolução da ultima questão

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    public int somar(int a, int b){
+        return a + b;
+    }
+
+    public double somar(double a, double b){
+        return a + b;
+    }
+
+    public long somar(long a, long b){
+        return a + b;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somar(1, 2);
+    }
+}
